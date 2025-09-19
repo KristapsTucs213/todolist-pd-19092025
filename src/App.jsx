ba7e0b052443cace584c376bce0f80a273968491
Your Name <you@example.com>
2025-09-19 14:08:55 +0300

    trying something

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,25 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [tasks, setTask] = useState(null)
+
+  async function clicker(){
+    const forma = document.getElementById("formm");
+    var formData = new FormData(forma);
+    console.log(Object.fromEntries(formData));
+  }
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>TO DO LIST</h1>
+    <form id='formm'>
+      <input type="text" name="input" id="input" />
+      <input type="submit" value="Submit" />
+    </form>
+      <button onClick={() => setTask((tasks) => tasks = "lol")}>
+        your task is {tasks}
+      </button>
     </>
   )
 }
